61686d24500a9972354b5e9e08c5d1a01d30de79
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-07 12:07:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,6 @@ def line(katz_deli)
     katz_deli.each_with_index{ |name, index|
       line_description << " #{index + 1}. #{name}"
     }
-    line_description << "."
   end
   puts line_description
 end

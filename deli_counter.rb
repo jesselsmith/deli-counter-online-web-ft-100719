720a79f2322db8257508547397b20402150be6ec
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-07 12:05:44 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,13 @@
+def line(katz_deli) 
+  line_description = "The line is currently"
+  if katz_deli.size == 0
+    line_description << " empty."
+  else
+    line_description << ":"
+    katz_deli.each_with_index{ |name, index|
+      line_description << " #{index + 1}. #{name}"
+    }
+    line_description << "."
+  end
+  line_description
+end

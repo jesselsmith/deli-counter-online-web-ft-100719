4f99ed82ff0458237b580915fedda382cc5431e7
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-07 12:11:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,8 @@ def line(katz_deli)
   end
   puts line_description
 end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in the line."
+end
